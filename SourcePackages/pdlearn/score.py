f57c5b9bce214619a37302174e193b5cb0066437
dompling <374779789@qq.com>
2023-04-03 15:35:49 +0800

    fix

--- SourcePackages/pdlearn/score.py
+++ SourcePackages/pdlearn/score.py
@@ -63,7 +63,7 @@ def get_score(cookies):
     jar = RequestsCookieJar()
     for cookie in cookies:
         jar.set(cookie['name'], cookie['value'])
-    total_json = requests.get("https://pc-api.xuexi.cn/open/api/score/get", cookies=jar,
+    total_json = requests.get("https://pc-api.xuexi.cn/delegate/score/get", cookies=jar,
                               headers={'Cache-Control': 'no-cache'}).content.decode("utf8")
     if not json.loads(total_json)["data"]:
         globalvar.pushprint("cookie过期，请重新登录", chat_id)
@@ -73,7 +73,7 @@ def get_score(cookies):
 
     total = int(json.loads(total_json)["data"]["score"])
     #userId = json.loads(total_json)["data"]["userId"]
-    user_info = requests.get("https://pc-api.xuexi.cn/open/api/user/info", cookies=jar,
+    user_info = requests.get("https://pc-api.xuexi.cn/delegate/user/info", cookies=jar,
                              headers={'Cache-Control': 'no-cache'}).content.decode("utf8")
     userId = json.loads(user_info)["data"]["uid"]
     userName = json.loads(user_info)["data"]["nick"]
@@ -83,7 +83,7 @@ def get_score(cookies):
     #                          headers={'Cache-Control': 'no-cache'}).content.decode("utf8")
     today = 0
     # today = int(json.loads(today_json)["data"]["score"])
-    score_json = requests.get("https://pc-proxy-api.xuexi.cn/api/score/days/listScoreProgress?sence=score&deviceType=2", cookies=jar,
+    score_json = requests.get("https://pc-proxy-api.xuexi.cn/delegate/score/days/listScoreProgress?sence=score&deviceType=2", cookies=jar,
                               headers={'Cache-Control': 'no-cache'}).content.decode("utf8")
     dayScoreDtos = json.loads(score_json)["data"]
     today = dayScoreDtos["totalScore"]
